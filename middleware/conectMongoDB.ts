e469a82a3e9a0a43749ff9d27d291336c1ce3e5e
mariorenanofc <mariorenan25@gmail.com>
2023-09-14 07:31:04 -0300

    feat(database): Middleware de verificação de conexão com o MongoDB
    Implementa um middleware que verifica a conexão com o banco de dados MongoDB antes de processar as solicitações para os endpoints. Se o banco já estiver conectado, o middleware permite que a solicitação prossiga; caso contrário, ele conecta ao banco de dados usando as informações da variável de ambiente DB_CONEXAO_STRING.
    
    #MongoDB #Middleware #ConexaoComBanco

--- middleware/conectMongoDB.ts
+++ middleware/conectMongoDB.ts
@@ -1,7 +1,9 @@
 import type {NextApiRequest, NextApiResponse, NextApiHandler} from 'next';
 import mongoose from 'mongoose';
+import {RespostaPadraoMsg} from '../types/respostaPadraoMsg';
 
-export const conectMongoDB = (handler : NextApiHandler) => (req: NextApiRequest, res: NextApiResponse) => {
+export const conectMongoDB =  (handler : NextApiHandler) => 
+    async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
 
     //Verificar se o banco está conectado, se estiver seguir para  o endpoint ou prox. middieware
     if(mongoose.connections[0].readyState){
@@ -10,4 +12,16 @@ export const conectMongoDB = (handler : NextApiHandler) => (req: NextApiRequest,
 
     //Se não esta conectado, vamos conectar, vamos obter a várialvel de ambiente preenchida do env
     const {DB_CONEXAO_STRING} = process.env;
+
+    //Se a env estiver vazia aborda o uso do  sistem e avisa o programador
+    if(!DB_CONEXAO_STRING){
+        return res.status(500).json({erro: 'ENV de configuração do  Banco, não  informada'});
+    }
+
+    mongoose.connection.on('connected',() => console.log('Banco Conectado'));
+    mongoose.connection.on('erro', error => console.log(`Ocorreu erro ao  conectar no Banco: ${error}`));
+    await mongoose.connect(DB_CONEXAO_STRING);
+
+    //Agora posso seguir para o endpoint, pois estou conectado no banco
+    return handler(req,  res);
 }
