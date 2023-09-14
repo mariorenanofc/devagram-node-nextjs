d0d3038187464b39e3a845cbc926e408db446ba3
mariorenanofc <mariorenan25@gmail.com>
2023-09-14 07:33:47 -0300

    feat(auth): Melhorias na rota de autenticação
    Refina a rota de autenticação no arquivo 'login.ts'. Agora, a rota verifica as credenciais de login e senha no método POST e retorna uma resposta apropriada. Além disso, utiliza o middleware de conexão com o MongoDB para garantir que a conexão com o banco de dados esteja estabelecida antes de processar as solicitações.
    
    #Autenticação #NextJS #MongoDB

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,15 +1,19 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
+import {conectMongoDB} from '../../middleware/conectMongoDB';
+import  {RespostaPadraoMsg} from '../../types/respostaPadraoMsg'
 
-export default (
+const endpointLogin = (
     req: NextApiRequest,
-    res: NextApiResponse
+    res: NextApiResponse<RespostaPadraoMsg>
 ) => {
     if(req.method === 'POST'){
         const {login, senha} = req.body;
         if(login === 'admin@admin123' && senha === 'pass123@pass') {
-            res. status(200).json({msg: 'Usuario autênticado com sucesso'})
+            return res. status(200).json({msg: 'Usuario autênticado com sucesso'})
         }
         return res.status(401).json({erro: 'Senha  ou usuario inválido!'})
     }
     return res.status(405).json({erro: 'Metodo informado não é válido'})
 }
+
+export default conectMongoDB(endpointLogin);
