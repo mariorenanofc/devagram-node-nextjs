170a377d0c777b0061cc5cd294ead121b7341f83
mariorenanofc <mariorenan25@gmail.com>
2023-09-20 08:20:54 -0300

    feat(models): Adiciona modelo de usuário (UsuarioModel)
    Nesta função ela cria um modelo de usuário chamado UsuarioModel no arquivo UsuarioModel.ts na pasta 'models'. Este modelo define a estrutura dos documentos de usuário no banco de dados MongoDB, incluindo campos como nome, email, senha, avatar e estatísticas. Ele também verifica se o modelo já existe para evitar duplicação.
    
    #Models #MongoDB #UsuarioModel

---
+++ models/UsuarioModel.ts
@@ -0,0 +1,13 @@
+import mongoose, {Schema} from 'mongoose';
+
+const usuarioSchema = new Schema({
+    nome: {type: String, require : true},
+    email: {type: String, require : true},
+    senha: {type: String, require : true},
+    avatar: {type: String, require : false},
+    seguidores: {type: Number, default : 0},
+    seguindo: {type: Number, default : 0},
+    publicacoes: {type: Number, default : 0},
+})
+
+export const UsuarioModel = (mongoose.models.usuarios || mongoose.model('usuarios', usuarioSchema));
