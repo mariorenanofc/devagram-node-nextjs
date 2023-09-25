4ac5fddff6259099517c9de2df0340ca8a2a814f
mariorenanofc <mariorenan25@gmail.com>
2023-09-25 08:03:50 -0300

    fix(models): Corrige nome de esquema em UsuarioModel
    No arquivo UsuarioModel.ts na pasta 'models', corrige um caractere no nome do esquema de 'UsuarioSchema' para 'UsuarioSchema'. Essa alteração é importante para garantir a consistência e a correção do código.
    
    #Models #Correcao #UsuarioModel

--- models/UsuarioModel.ts
+++ models/UsuarioModel.ts
@@ -1,6 +1,6 @@
 import mongoose, {Schema} from 'mongoose';
 
-const usuarioSchema = new Schema({
+const UsuarioSchema = new Schema({
     nome: {type: String, require : true},
     email: {type: String, require : true},
     senha: {type: String, require : true},
@@ -10,4 +10,4 @@ const usuarioSchema = new Schema({
     publicacoes: {type: Number, default : 0},
 })
 
-export const UsuarioModel = (mongoose.models.usuarios || mongoose.model('usuarios', usuarioSchema));
+export const UsuarioModel = (mongoose.models.usuarios || mongoose.model('usuarios', UsuarioSchema));
