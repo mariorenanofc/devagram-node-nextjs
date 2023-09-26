95cd151ea1e332ca50ea6f99c90da0e3a29b1fb3
mariorenanofc <mariorenan25@gmail.com>
2023-09-26 08:23:14 -0300

    feat(api): Adiciona verificação de duplicidade de dados no endpoint de cadastro
    No arquivo cadastro.ts na pasta 'pages/api', foram adicionadas verificações para evitar a duplicação de dados ao cadastrar um novo usuário. Agora, o sistema verifica se o email ou nome já foram cadastrados anteriormente e retorna erros apropriados se houver duplicação.
    
    #API #Cadastro #VerificacaoDuplicidade #NextJS #MongoDB

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -3,6 +3,7 @@ import type { RespostaPadraoMsg } from "../../types/respostaPadraoMsg";
 import type { cadastroRequisicao } from "../../types/cadastroRequisicao";
 import { UsuarioModel } from "../../models/UsuarioModel";
 import { conectMongoDB } from "@/middleware/conectMongoDB";
+import md5 from 'md5';
 
 const endpointCadastro = async (
   req: NextApiRequest,
@@ -27,9 +28,25 @@ const endpointCadastro = async (
     if (!usuario.senha || usuario.senha.length < 4) {
       return res.status(400).json({ erro: "Senha Invalida" });
     }
+    //Validação de usuario em duplicidade
+
+    const usuariosComMesmoEmail = await UsuarioModel.find({email: usuario.email});
+    if(usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0){
+      return res.status(400).json({ erro: "Esse Email já foi cadastrado!" });
+    }
+
+    const usuariosComMesmoNome = await UsuarioModel.find({nome: usuario.nome});
+    if(usuariosComMesmoNome && usuariosComMesmoNome.length > 0){
+      return res.status(400).json({ erro: "Algum usuario já utilizou esse nome!" });
+    }
 
     //Salvar Bando de Dados
-    await UsuarioModel.create(usuario);
+    const usuarioASerSalvo = {
+      nome: usuario.nome,
+      email: usuario.email,
+      senha: md5(usuario.senha)
+    }
+    await UsuarioModel.create(usuarioASerSalvo);
 
     return res.status(200).json({ msg: "Conta criada com sucesso!" });
   }
