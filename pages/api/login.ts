49b58adcc90e9231da3b13be7e0320ccb2c91d45
mariorenanofc <mariorenan25@gmail.com>
2023-09-28 07:02:57 -0300

    feat(auth): Adiciona autenticação JWT ao endpoint de login
    Adiciona a autenticação JWT ao endpoint de login para gerar um token JWT após a autenticação bem-sucedida do usuário. O token é assinado com a chave JWT fornecida na variável de ambiente MINHA_CHAVE_JWT.
    
    #Autenticacao #JWT #login #MINHA_CHAVE_JWT

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -3,19 +3,34 @@ import {conectMongoDB} from '../../middleware/conectMongoDB';
 import  {RespostaPadraoMsg} from '../../types/respostaPadraoMsg';
 import md5 from 'md5';
 import { UsuarioModel } from '@/models/UsuarioModel';
+import jwt from 'jsonwebtoken';
+import { loginResposta } from '@/types/loginResposta';
 
 const endpointLogin = async (
     req: NextApiRequest,
-    res: NextApiResponse<RespostaPadraoMsg>
+    res: NextApiResponse<RespostaPadraoMsg | loginResposta>
 ) => {
+
+    const {MINHA_CHAVE_JWT} = process.env
+    if(!MINHA_CHAVE_JWT){
+        return res.status(500).json({erro : 'ENV jwt não informado!'})
+    }
+
     if(req.method === 'POST'){
 
         const {login, senha} = req.body;
 
         const usuariosEncontrados = await UsuarioModel.find({email: login, senha: md5(senha)});
         if(usuariosEncontrados && usuariosEncontrados.length > 0) {
-            const usuarioEncontrado = usuariosEncontrados[0];            
-            return res. status(200).json({msg: `Usuario ${usuarioEncontrado.nome} autênticado com sucesso`})
+            const usuarioEncontrado = usuariosEncontrados[0];
+
+            const token = jwt.sign({_id : usuarioEncontrado._id}, MINHA_CHAVE_JWT)
+
+            return res. status(200).json({
+                nome : usuarioEncontrado.nome,
+                email : usuarioEncontrado.email,
+                token
+            });
         }
         return res.status(401).json({erro: 'Senha  ou usuario inválido!'})
     }
